d4cfbfc0d213d1f7323a61e79ec91cd9ac384e08
El Parche DEV <tu.email@ejemplo.com>
2025-02-18 00:16:18 -0500

    feat: all providers integrated

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { LibraryProvider } from './context/LibraryContext';
+import { UIProvider } from './context/UIContext';
 import Layout from './components/Layout';
 import Routes from './Routes';
+import NotificationSystem from './components/Notifications/NotificationSystem';
 
 const App = () => {
   return (
     <Router>
-      <LibraryProvider>
-        <Layout>
-          <Routes />
-        </Layout>
-      </LibraryProvider>
+      <UIProvider>
+        <LibraryProvider>
+          <Layout>
+            <Routes />
+            <NotificationSystem />
+          </Layout>
+        </LibraryProvider>
+      </UIProvider>
     </Router>
   );
 };
